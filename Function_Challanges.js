61e18a588d741f320262ac59d8a3b8ba710e43dd
Farhan-Khan7 <fk5518744@gmail.com>
2025-03-07 12:54:56 +0530

    I have Solve 3 Challanges in Java Script

---
+++ Function_Challanges.js
@@ -0,0 +1,53 @@
+// PHASE 0 CHALLANGE 1 
+
+// You and your friend are baking cupcakes for a party. You've filled up two trays, each with a certain number of cupcakes. You want to know the total number of cupcakes you have before you frost them.
+
+// Challenge
+
+// Create a function that adds two numbers (representing cupcakes on each tray) and returns the total.
+
+// You just need to implement the totalCupcakes function
+
+function totalCupcakes(trayOne, trayTwo) {
+    // Return the sum of trayOne and trayTwo
+    return trayOne + trayTwo;
+    
+}
+console.log(totalCupcakes(12 , 20))  
+
+
+
+// You have a basket of apples, but you decide to give some away to your neighbor. How many apples are left in your basket?
+
+// Challenge
+
+// Write a function that subtracts the number of apples given away from the total apples you started with, then returns the remaining apples.
+
+// Your code starts here
+// You just need to implement the totalChocolateBars function
+
+// Your code starts here
+// You just need to implement the remainingApples function
+function remainingApples(totalApples, givenAway) {
+    // Return totalApples minus givenAway
+    return totalApples - givenAway ;
+    
+  }
+console.log(remainingApples(50 , 10));  
+
+
+// You're packing chocolate bars into gift boxes. Each box must contain the same number of chocolate bars. How many chocolate bars do you need for all the boxes?
+
+// Challenge
+
+// Create a function that multiplies two numbers (chocolate bars per box and number of boxes) and retums the total number of chocolate bars needed.
+
+
+// Your code starts here
+// You just need to implement the totalChocolateBars function
+function totalChocolateBars(barsPerBox, numberOfBoxes) {
+    // Return barsPerBox multiplied by numberOfBoxes
+    return barsPerBox * numberOfBoxes;
+    
+  }
+console.log(totalChocolateBars(5 , 10));
