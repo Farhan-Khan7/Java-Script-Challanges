54d0c7dc2db990a5c260c46be623b3bd27e1b04f
Farhan-Khan7 <fk5518744@gmail.com>
2025-05-16 22:29:34 +0530

    1st changes solve

--- Conditional_Challanges.js
+++ Conditional_Challanges.js
@@ -6,4 +6,18 @@
 //   }
 
 // }
-console.log(checkEvenOdd(8))
+// console.log(checkEvenOdd(8))
+
+
+function findLargest(a, b, c) {
+  // Return the largest among a, b, and c
+
+  if(a>=b && a>=c){
+    return a
+  }else if(b>=c && b>=a){
+    return b
+  }else{
+    return c
+  }
+}
+console.log(findLargest(8,5,6))
