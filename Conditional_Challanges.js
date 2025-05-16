f737b54f63f49588b3a244f7b68a003008fdcaf2
Farhan-Khan7 <fk5518744@gmail.com>
2025-05-16 22:34:33 +0530

    2nd changes solve

--- Conditional_Challanges.js
+++ Conditional_Challanges.js
@@ -1,23 +1,23 @@
-// function checkEvenOdd(num){
-//   if(num % 2 == 0){
-//     return "Even";
-//   }else{
-//     return "Odd"
-//   }
-
-// }
-// console.log(checkEvenOdd(8))
-
-
-function findLargest(a, b, c) {
-  // Return the largest among a, b, and c
-
-  if(a>=b && a>=c){
-    return a
-  }else if(b>=c && b>=a){
-    return b
+function checkEvenOdd(num){
+  if(num % 2 == 0){
+    return "Even";
   }else{
-    return c
+    return "Odd"
   }
+
 }
-console.log(findLargest(8,5,6))
+console.log(checkEvenOdd(0))
+
+
+// function findLargest(a, b, c) {
+//   // Return the largest among a, b, and c
+
+//   if(a>=b && a>=c){
+//     return a
+//   }else if(b>=c && b>=a){
+//     return b
+//   }else{
+//     return c
+//   }
+// }
+// console.log(findLargest(5,5,5))
