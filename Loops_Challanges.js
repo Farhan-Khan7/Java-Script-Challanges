beba04f5026d9c194f00b4c8730a942f8c7e012f
Farhan-Khan7 <fk5518744@gmail.com>
2025-05-18 22:36:42 +0530

    I have solve 2nd of loops

---
+++ Loops_Challanges.js
@@ -0,0 +1,24 @@
+// function distributeGifts(totalGifts, friends) {
+//   // write your code here
+//   let distribute_gifts = 0;
+
+//   for(let i=0; i<friends; i++){
+//     if(totalGifts>0){
+//     distribute_gifts++ 
+//     totalGifts--;
+//     }
+//   }
+//   return distribute_gifts;
+// }
+// console.log(distributeGifts(10 , 5))
+
+function countApples(apples) {
+
+    let i = 0
+    for(let i = 0; i<= apples; i++){
+        
+    }
+    
+ 
+}
+console.log(countApples(3))
