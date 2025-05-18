2233f0741afe391726e2dc269f4f5bd6f08cd512
Farhan-Khan7 <fk5518744@gmail.com>
2025-05-18 15:54:41 +0530

    I have solve 4th Challange

--- Conditional_Challanges.js
+++ Conditional_Challanges.js
@@ -12,20 +12,56 @@
 // console.log(user)
 
 
-function findLargest() {
-  // Return the largest among a, b, and c
+// function findLargest() {
+//   // Return the largest among a, b, and c
 
-  let a = prompt("Enter Fisrt num")
-  let b = prompt("Enter Second num")
-  let c = prompt("Enter Third num")
+//   let a = prompt("Enter Fisrt num")
+//   let b = prompt("Enter Second num")
+//   let c = prompt("Enter Third num")
 
-  if(a>=b && a>=c){
-    return `The Largest number is ${a}`
-  }else if(b>=c && b>=a){
-    return `The Largest number is ${b}`
-  }else{
-    return `The Largest number is ${c}`
+//   if(a>=b && a>=c){
+//     return `The Largest number is ${a}`
+//   }else if(b>=c && b>=a){
+//     return `The Largest number is ${b}`
+//   }else{
+//     return `The Largest number is ${c}`
+//   }
+// }
+// let result = alert(findLargest())
+// console.log(result)
+
+
+  // function checkVotingEligibility() {
+  //   // Return "Eligible" if age is 18 or more, otherwise return "Not Eligible"
+  //   let age = prompt("Enter Your Age : ")
+  //   if(age >= 18){
+  //     return "Eligible"
+  //   }else{
+  //     return "Not Eligible"
+  //   }
+
+  // }
+  // let result = alert(checkVotingEligibility())
+  // console.log(result)
+
+  
+  function calculateGrade() {
+    // Return grade based on the marks
+    let marks = prompt("Enter Your Marks")
+
+    if(marks >= 90){
+      return "Grade A"
+    }else if(marks >= 80 && marks < 89){
+      return "Grade B"
+    }else if(marks >= 70 && marks < 79){
+      return "Grade C"
+    }else if(marks >= 60 && marks < 69){
+      return "Grade D"
+    }else{
+      return "Grade F"
+    }
+
   }
-}
-let result = alert(findLargest())
+
+let result = alert(calculateGrade())
 console.log(result)
