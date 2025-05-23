ca2c1d885f28975ae2b075bf28ef2e26ac9c43f8
Farhan-Khan7 <fk5518744@gmail.com>
2025-05-23 16:05:05 +0530

    I have solve 6 challange

--- Arrays_Challanges.js
+++ Arrays_Challanges.js
@@ -15,10 +15,36 @@
 // console.log(eatCandy(["lolipop" , "Gum" , "Chocolate"]))
 
 // You just need to implement the addPuppy function
-function addPuppy(queue, puppyName) {
-    // Add puppyName at the beginning of queue and return updated queue
-    queue.unshift(puppyName)
-    return queue;
-}
+// function addPuppy(queue, puppyName) {
+//     // Add puppyName at the beginning of queue and return updated queue
+//     queue.unshift(puppyName)
+//     return queue;
+// }
 
-console.log(addPuppy(["Tom","Jerry","Spike"],"Max"))
+// console.log(addPuppy(["Tom","Jerry","Spike"],"Max"))
+
+
+// You just need to implement the sortBooks function
+// function sortBooks(books) {
+//     // Sort the books alphabetically and return the updated list
+//     books.sort()
+//     return books
+// }
+// console.log(sortBooks(["Maths","English","Science","History"]))
+
+
+// You just need to implement the removeStudent function
+// function removeStudent(bus) {
+//     // Remove the first student and return the updated bus list
+//     bus.shift()
+//     return bus;
+// }
+
+// console.log(removeStudent(["John","Sarah","Mike","Emma"]))
+
+
+// function findPhone(items) {
+//     // Return the index of "Phone" in the items array
+//     return items.indexOf("Phone");
+// }
+// console.log(findPhone(["Wallet","Keys","Glasses","Phone"]))
