1a0daba6364560d95a3e5aa9e1043e8a65464f45
Farhan-Khan7 <fk5518744@gmail.com>
2025-05-23 15:31:54 +0530

    I start sovlve array challanges

--- Arrays_Challanges.js
+++ Arrays_Challanges.js
@@ -1,8 +1,15 @@
-function addGuest(guestList, newGuest) {
-    // Add the newGuest to guestList and return the updated list
-    guestList.push(newGuest)
+// function addGuest(guestList, newGuest) {
+//     // Add the newGuest to guestList and return the updated list
+//     guestList.push(newGuest)
 
-    return guestList
+//     return guestList
+// }
+
+// console.log(addGuest(["Anirudh","Mukul"]," Radhika"))
+
+function eatCandy(candyJar) {
+    // Remove the last candy from the jar and return the updated jar
+    candyJar.pop()
+    return candyJar;
 }
-
-console.log(addGuest(["Anirudh","Mukul"]," Radhika"))
+console.log(eatCandy(["lolipop" , "Gum" , "Chocolate"]))
