c161fccdac904930a9b0cb2f941afd9a47f16413
Farhan-Khan7 <fk5518744@gmail.com>
2025-05-24 22:14:14 +0530

    I have create guess the random number game

---
+++ Game.js
@@ -0,0 +1,21 @@
+const max = prompt("Enter the maximum number");
+const random_number = Math.floor(Math.random()*max)+1;
+
+let guess = prompt("Guessing the number");
+
+while(true){
+    if(guess == "quit"){
+        console.log("You are QUIT now")
+        break;
+    }
+
+
+    if(guess == random_number){
+        console.log("You are guessing right random Number ")
+        break;
+    }else if(random_number > guess){
+        guess = prompt("your number  is too small ! Try Again ")
+    }else{
+        guess = prompt("your number  is too large ! Try Again ")
+    }
+}
