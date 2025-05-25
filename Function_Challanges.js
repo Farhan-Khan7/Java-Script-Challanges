de7b17fb637d84ac589633cf7d20e98cd602541e
Farhan-Khan7 <fk5518744@gmail.com>
2025-05-25 12:18:26 +0530

    I have done this challanges

--- Function_Challanges.js
+++ Function_Challanges.js
@@ -1,5 +1,23 @@
-function randomNumber() {
-    let Random = Math.floor(Math.random()*6)+1;
-    return Random;
+// function randomNumber() {
+//     let Random = Math.floor(Math.random()*6)+1;
+//     return Random;
+// }
+// console.log(randomNumber())
+
+// function table(tbl) {
+//     for(let i=1; i<=10; i++){
+//         console.log(`${tbl} * ${i} = ${tbl*i}`);
+//     }
+// }
+
+// table(2);
+
+function AddString(str){
+    let SumString = '';
+    for(let i=0; i<str.length; i++){
+        SumString += str[i];
+    }
+    return SumString;
 }
-console.log(randomNumber())
+
+console.log(AddString(["F","A","R","H","A","N"]))
