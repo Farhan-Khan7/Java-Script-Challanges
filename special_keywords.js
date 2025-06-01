4c0b38d2078766ca90e849388d27dce1ff10c4bf
Farhan-Khan7 <fk5518744@gmail.com>
2025-06-01 12:04:23 +0530

    I have done code

---
+++ special_keywords.js
@@ -0,0 +1,32 @@
+// const obj = {
+//     first_name: "Farhan",
+//     last_name: "Khan",
+//     name_concat() {
+//         let concat = this.first_name + " " + this.last_name;
+//         // console.log(this)
+//     }
+
+// }
+// obj.name_concat()
+
+
+// console.log("Hey Farhan !") 
+// setTimeout(() => {
+//     console.log("Monaco")
+//     console.log("Canada")
+//     console.log("Chicago")
+// } , 1000 )
+
+
+// console.log("Welcome to")
+
+// let id = setInterval(() => {
+//     console.log("Hello I am Future Trillioner")
+// } , 3000)
+
+// console.log(id)
+
+
+let square = n => (n*n)
+
+console.log(square(2))
