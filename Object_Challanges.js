e8533a47f1262c9f2a3f152ca881062c58601b35
Farhan-Khan7 <fk5518744@gmail.com>
2025-06-01 12:47:44 +0530

    done

---
+++ Object_Challanges.js
@@ -0,0 +1,21 @@
+// You just need to implement the createStudentProfile function
+// function createStudentProfile(name, age, grade) {
+//   // Return an object with name, age, and grade
+
+//     if(typeof(name) !== "string" || typeof(grade) !== "string" || typeof(age) !== "number" || age > 5 ){
+
+//     }else{
+//         return "Invalid input"
+//     }
+
+//     return {name , age , grade}
+
+
+// }
+
+// console.log(createStudentProfile("Farhan",19,"A"))
+
+
+function addCarColor(car, color) {
+  // Add color property to the car object
+}
